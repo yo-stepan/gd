d69ab22fdd1a52d59b2831ee016a0f5e33f7c2fa
Stepan Smirnov <stepan@microfunk.ru>
2012-11-14 18:21:19 +0400

    header upd

---
+++ blocks/header/header.js
@@ -0,0 +1,25 @@
+$.widget("block.header", {
+
+    _create: function() {
+        var block = this,
+            $block = this.element,
+            bOffset = $block.offset().top;
+
+        block.$logo = $block.find('.header__logo');
+
+        block.$blast = $('<div />').addClass('header_blast').css({'height': $block.height()});
+
+        // фиксируем блок при прокрутке страницы
+        $(window).on('scroll', function(){
+            if ($(window).scrollTop() >= bOffset) {
+                $block.addClass('header_fixed');
+                $block.after(block.$blast);
+            } else {
+                $block.removeClass('header_fixed');
+                block.$blast.remove();
+            }
+        });
+
+    }
+
+});
