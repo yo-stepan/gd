1a04bb9e0a18a91aa5d4b7cb23b411d36652f49c
Stepan Smirnov <stepan@microfunk.ru>
2012-11-15 13:53:42 +0400

    home changes

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,18 +5,20 @@ $.widget("block.header", {
             $block = this.element,
             bOffset = $block.offset().top;
 
+        console.log(bOffset);
+
         block.$logo = $block.find('.header__logo');
 
-        block.$blast = $('<div />').addClass('header_blast').css({'height': $block.height()});
+        //block.$blast = $('<div />').addClass('header_blast').css({'height': $block.height()});
 
         // фиксируем блок при прокрутке страницы
         $(window).on('scroll', function(){
             if ($(window).scrollTop() >= bOffset) {
                 $block.addClass('header_fixed');
-                $block.after(block.$blast);
+                //$block.after(block.$blast);
             } else {
                 $block.removeClass('header_fixed');
-                block.$blast.remove();
+                //block.$blast.remove();
             }
         });
 
