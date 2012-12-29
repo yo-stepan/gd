efaac6b4aaf4c7ed00a967c5b0c5e07136431367
Stepan Smirnov <stepan@microfunk.ru>
2012-12-29 14:05:23 +0400

    upd

--- blocks/feature/feature_map.js
+++ blocks/feature/feature_map.js
@@ -1,24 +1,24 @@
 $.widget("block.feature_map", {
 
     _create: function() {
-        console.log('go feature map');
-
-        var block = this,
-            $block = this.element;
-
-        block.$mapShow = $block.find('.feature__mapShow');
-        block.$map = $block.find('.map');
-
-        block.$mapShow.on('click', function() {
-            //$block.addClass('feature_open');
-            $block.animate({
-                height: 500
-            }, 300, function() {
-                block.$map.map('expand');
-            });
-
-            return false;
-        });
+//        console.log('go feature map');
+//
+//        var block = this,
+//            $block = this.element;
+//
+//        block.$mapShow = $block.find('.feature__mapShow');
+//        block.$map = $block.find('.map');
+//
+//        block.$mapShow.on('click', function() {
+//            //$block.addClass('feature_open');
+//            $block.animate({
+//                height: 500
+//            }, 300, function() {
+//                block.$map.map('expand');
+//            });
+//
+//            return false;
+//        });
 
     }
 
