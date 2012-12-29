2db5f5ec09cdb2ac0359a09988d58ce4a9f39a4e
Stepan Smirnov <stepan@microfunk.ru>
2012-12-29 14:10:36 +0400

    upd

--- blocks/map/map.js
+++ blocks/map/map.js
@@ -11,6 +11,29 @@ $.widget("block.mapmap", {
         console.log('go map');
 
 
+        var block = this,
+            $block = this.element,
+            mapOptions = {
+                center: new google.maps.LatLng(59.928184,30.333149),
+                zoom: 16,
+                disableDefaultUI: true,
+                mapTypeId: google.maps.MapTypeId[block.options.mapType]
+            };
+
+        var styles = [
+            {
+                "stylers": [
+                    { "visibility": "simplified" },
+                    { "weight": 0.1 },
+                    { "saturation": -69 },
+                    { "lightness": 17 },
+                    { "gamma": 1.02 }
+                ]
+            }
+        ];
+
+        block.map = new google.maps.Map(this.element[0],  mapOptions);
+        block.map.setOptions({'styles': styles});
     },
 
     _init: function() {
